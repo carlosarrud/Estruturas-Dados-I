e803182e406aeac166d0049ccc420b9edc986224
carlosarrud <carlosdesbravador777@gmail.com>
2024-07-30 14:23:32 -0300

    Create Questão1.c

---
+++ Classe/Lista3/Questão1.c
@@ -0,0 +1,51 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void preencher_vetor(int tamanho, int *vetor){
+    int count;
+    
+    printf("\nInforme os elementos do vetor: \n");
+
+    for(count = 0; count < tamanho; count++){
+        scanf("%d", &vetor[count]);
+    }
+}
+
+void imprime (int tamanho, int *vetor){
+    int count;
+    printf("Vetor inverso: \t");
+    for (count = tamanho-1; count >= 0; count--)
+    {
+        printf("%d \t", vetor[count]);
+    }
+}
+
+int * aloca_vetor(int tamanho){
+    int *vetor = (int*) malloc(tamanho* sizeof (int));
+    if (vetor == NULL){
+        printf("No memory\n");
+        exit(1);
+    } else{
+        printf("Vetor alocado com sucesso\n");
+    }
+    return vetor;
+}
+
+int main (void){
+    int tamanho;
+    printf("Informe o tamanho do vetor: \t");
+    scanf("%d", &tamanho);
+    
+    //Aloca vetor
+    int * vetor = aloca_vetor(tamanho);
+    
+    //Elementos do vetor
+    preencher_vetor (tamanho, vetor);
+
+    //Imprimei vetor inverso
+    imprime(tamanho, vetor);
+
+    free(vetor);
+    return 0;
+}
